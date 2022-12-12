bd3b7463c2cd6cc5973f88644cfed96fc7cb9ba3
lanchero <juan.lancheros662@misena.edu.co>
2022-12-12 09:10:13 -0500

    carpeta entities

---
+++ Entities/Models/Agency.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Agency
+    {
+        [Key]
+        [Column("AgencyId")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Agency name is required field")]
+        [MaxLength(60, ErrorMessage = "Maximun lenght for the name is 60 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Agency direction is required field")]
+        [MaxLength(120, ErrorMessage = "Maximun lenght for the direction is 120 characters")]
+        public string Direction { get; set; }
+
+        [Required(ErrorMessage = "Agency poblation is required field")]
+        [MaxLength(60, ErrorMessage = "Maximun lenght for the poblation is 60 characters")]
+        public  string Poblation { get; set; }
+
+
+
+
+        [InverseProperty("Agency")]
+        public ICollection<Rent> Rent { get; set; } 
+        
+        
+        [InverseProperty("Agency")]
+        public ICollection<Motorcycle> Motorcycle { get; set; }
+
+
+        [InverseProperty("Agency")]
+        public ICollection<Phones> Phone { get; set; }
+
+
+    }
+}
---
+++ Entities/Models/Rent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Rent
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public int IdMotorcycle { get; set; }
+
+        public int IdAgency { get; set; }
+
+        [MaxLength(9, ErrorMessage = "Maximun lenght for the DNIClient is 9 characters")]
+        public string DNIClient { get; set; }
+
+        public DateTime dateI { get; set; }
+
+        public DateTime dateF { get; set; }
+
+        public DateTime dateR { get; set; }
+
+        public int Price { get; set; }
+
+        public string Paidout { get; set; }
+
+
+       
+
+        [ForeignKey("DNIClient")]
+        [InverseProperty("Rent")]
+        public  Client Client { get; set; }
+
+
+        [ForeignKey("IdAgency")]
+        [InverseProperty("Rent")]
+        public  Agency Agency { get; set; }
+
+        [ForeignKey("IdMotorcycle")]
+        [InverseProperty("Rent")]
+        public Motorcycle Motorcycle { get; set; }
+
+    }
+}
