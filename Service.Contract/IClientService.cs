79fa77ff15434c798050104b4bba80862fffb645
lanchero <juan.lancheros662@misena.edu.co>
2022-12-12 09:17:24 -0500

    carpeta service.contract

---
+++ Service.Contract/IClientService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using Shared;
+using Shared.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contract
+{
+    public interface IClientService
+    {
+        IEnumerable<ClientDto> GetAllClients(bool trackChanges);
+    }
+}
---
+++ Service.Contract/IMotorcycleService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Shared.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contract
+{
+    public interface IMotorcycleService
+    {
+        IEnumerable<MotorcycleDto> GetAllMotorcycles(bool trackChanges);
+    }
+}
